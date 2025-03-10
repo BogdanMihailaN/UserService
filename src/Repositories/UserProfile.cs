c8f1206653d58e6347c7f7289b1121279240de18
Bogdan <Bogdan@DESKTOP-IENSPC0>
2025-03-10 17:19:11 +0200

    small change

--- src/Repositories/UserProfile.cs
+++ src/Repositories/UserProfile.cs
@@ -4,13 +4,13 @@ using Domain.Models;
 namespace Repositories
 {
     public class UserProfile : Profile
-{
-    public UserProfile()
     {
-        CreateMap<Domain.Entities.User, UserModel>();
-        CreateMap<UserModel, Domain.Entities.User>();
-        CreateMap<RoleModel, Domain.Entities.Role>();
-        CreateMap<Domain.Entities.Role, RoleModel>();
+        public UserProfile()
+        {
+            CreateMap<Domain.Entities.User, UserModel>();
+            CreateMap<UserModel, Domain.Entities.User>();
+            CreateMap<RoleModel, Domain.Entities.Role>();
+            CreateMap<Domain.Entities.Role, RoleModel>();
+        }
     }
-}
 }
